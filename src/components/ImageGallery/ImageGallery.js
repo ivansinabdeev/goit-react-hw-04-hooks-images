edaaf62eab13c8ba4e90147145d2c99db403488b
ivansinabdeev <ivansinabdeev@gmail.com>
2021-10-14 19:25:25 +0200

    fixed on hooks

--- src/components/ImageGallery/ImageGallery.js
+++ src/components/ImageGallery/ImageGallery.js
@@ -1,37 +1,34 @@
-import React, { Component } from "react";
+import { useEffect } from "react";
 import propTypes from "prop-types";
 
 import ImageGalleryItem from "./ImageGalleryItem";
 import s from "./ImageGallery.module.css";
 
-class ImageGallery extends Component {
-  static defaultProps = { imagesArray: propTypes.array };
-  static propTypes = {
-    loader: propTypes.func,
-    imagesArray: propTypes.array,
-    modalFn: propTypes.func,
-  };
-  componentDidMount() {
-    this.props.loader(true);
-  }
-  render() {
-    return (
-      <ul className={s.ImageGallery}>
-        {this.props.imagesArray.map(
-          ({ id, webformatURL, tags, largeImageURL }) => {
-            return (
-              <ImageGalleryItem
-                key={id}
-                imageLink={webformatURL}
-                imagAlt={tags}
-                largeImageURL={largeImageURL}
-                modalFn={this.props.modalFn}
-              />
-            );
-          }
-        )}
-      </ul>
-    );
-  }
+export default function ImageGallery({ loader, imagesArray, modalFn }) {
+  useEffect(() => {
+    loader(true);
+  });
+  return (
+    <ul className={s.ImageGallery}>
+      {imagesArray.map(({ id, webformatURL, tags, largeImageURL }) => {
+        return (
+          <ImageGalleryItem
+            key={id}
+            imageLink={webformatURL}
+            imagAlt={tags}
+            largeImageURL={largeImageURL}
+            modalFn={modalFn}
+          />
+        );
+      })}
+    </ul>
+  );
 }
-export default ImageGallery;
+
+ImageGallery.defaultProps = { imagesArray: propTypes.array };
+
+ImageGallery.propTypes = {
+  loader: propTypes.func,
+  imagesArray: propTypes.array,
+  modalFn: propTypes.func,
+};
